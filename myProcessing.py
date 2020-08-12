71c30ef6ea964cdba2c4b2200c8f5c90cad32a84
X-CCS <CCS695146667@163.com>
2020-08-12 11:39:00 +0800

    update

--- myProcessing.py
+++ myProcessing.py
@@ -45,16 +45,9 @@
 #     play_audio(out, sr)
 
 """
-audio_editor 去静默
-audio_noise_remover 语音降噪，降低环境噪声。
-audio_normalizer 语音正则化，去除音量低的音频段（去除静音），调节音量
-TODO:将这三个整合成一个 加上spleeter
-"""
-
-"""
 inpath 输入处理的音频
 outpath 输出处理完的音频
-
+purpose 原声->去噪->声音正则化->去静默
 """
 def audio_processing(inpath,outpath):
     import aukit
@@ -63,23 +56,13 @@ def audio_processing(inpath,outpath):
     from aukit.audio_player import play_sound, play_audio
     from aukit import audio_editor as aed
     from aukit.audio_io import load_wav, save_wav
-    # inpath = r"1_test.wav"
-    # outpath = r"./myProcessing_test.wav"
     """run_noise_remover"""""
     wav, sr = aukit.load_wav(inpath, with_sr=True)
     out = aukit.remove_noise(wav)
-    # save_wav(out, outpath, sr) # 保存音频
-    # aukit.play_audio(out) # 播放音频
     """run_normalizer"""""
-    # inpath = r"提取人声_2.wav"
-    # inpath = r"提取人声_3.wav"
-    # wav, sr = aukit.load_wav(inpath, with_sr=True)
     out = ano.remove_silence(wav)
     out = ano.tune_volume(wav, target_dBFS=-10)
-    # save_wav(out, outpath, sr) # 保存音频
-    # play_sound(out, sr) # 播放音频
     """run_editor"""""
-    # wav, sr = aukit.load_wav(inpath, with_sr=True)
     aud = aed.wav2audiosegment(wav, sr)
     out = aed.strip_audio(aud)
     wav = aed.audiosegment2wav(out)
@@ -87,10 +70,10 @@ def audio_processing(inpath,outpath):
     out = aed.remove_silence_wave(wav, sr=sr)
     out = aed.strip_silence_wave(out, sr=sr)
     save_wav(out, outpath, sr)  # 保存音频
+    # play_sound(out, sr) # 播放音频
     print(len(wav), len(out))
-    # play_audio(out, sr)
-
-    return outpath
+    print("done")
+    # return outpath
 
 if __name__=="__main__":
     inpath = r"./提取人声_1.wav"
